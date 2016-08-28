9783a2af67b76f2d218ba35efe140be2a3b5933a
priyankamehta2690 <pmehta11@uncc.edu>
2016-08-28 03:23:24 -0400

    Adding Message A Niner Client

---
+++ MessageANinerTestProject/ScreensUnitTestClass.cs
@@ -0,0 +1,132 @@
+﻿using MessageANiner.Entities;
+using MessageANiner.Home;
+using messageSpecs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MessageANinerTestProject
+{
+    [TestClass]
+    public class ScreensUnitTestClass
+    {
+        ProfileInfo userDetails = new ProfileInfo();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            userDetails.userID = "1";
+            userDetails.userName = "Vaibhav";
+            userDetails.verified = true;
+            userDetails.lastName = "Sapkal";
+        }
+
+        [TestMethod]
+        public void TestUpdateContacts_NewUserAdded()
+        {
+            HomePage screen = new HomePage();
+            screen.UserDetails = userDetails;
+            messageSpecs.FriendStatusMessage x = new FriendStatusMessage();
+            x.sender = "1";
+            x.UserStatus = FriendStatusMessage.LoggedOnStatus.Online;
+            screen.updateContacts(x.getMessageString());
+            messageSpecs.FriendStatusMessage y = new FriendStatusMessage();
+            y.sender = "2";
+            y.UserStatus = FriendStatusMessage.LoggedOnStatus.Online;
+            screen.updateContacts(y.getMessageString());
+
+            int check1 = screen.ContactsList.RowCount;
+            Assert.AreEqual(3, check1);
+            this.releaseResources(screen);
+
+        }
+
+        [TestMethod]
+        public void TestUpdateContacts_UserStatusChange()
+        {
+            HomePage screen = new HomePage();
+            screen.UserDetails = userDetails;
+            messageSpecs.FriendStatusMessage y = new FriendStatusMessage();
+            y.sender = "2";
+            y.UserStatus = FriendStatusMessage.LoggedOnStatus.Offline;
+            screen.updateContacts(y.getMessageString());
+
+            object c = screen.ContactsList.Controls[0];
+            object status = ((MessageANiner.UserControls.ContactControl)(c)).avilability;
+            Assert.AreEqual(MessageANiner.UserControls.ContactControl.Status.Offline, status);
+            		
+            y.UserStatus = FriendStatusMessage.LoggedOnStatus.Online;
+            screen.updateContacts(y.getMessageString());
+
+            status = ((MessageANiner.UserControls.ContactControl)(c)).avilability;
+            Assert.AreEqual(MessageANiner.UserControls.ContactControl.Status.Online, status);
+
+            this.releaseResources(screen);
+        }
+
+        [TestMethod]
+        public void TestUpdateMessage()
+        {
+            HomePage screen = new HomePage();
+            screen.UserDetails = userDetails;
+            messageSpecs.FriendStatusMessage y = new FriendStatusMessage();
+            y.sender = "0000000002";
+            y.UserStatus = FriendStatusMessage.LoggedOnStatus.Online;
+            screen.updateContacts(y.getMessageString());
+
+            messageSpecs.TextMessage tm = new TextMessage();
+            tm.sender = "0000000001";
+            tm.receiver = "0000000002";
+            tm.TextToSend = "ABCD";
+            screen.updateMessages(tm.getMessageString());
+
+            object isChatpresent = screen.openChatWindows.ContainsKey(long.Parse(tm.receiver));
+            Assert.AreEqual(true, isChatpresent);
+
+            object chatWindow = screen.openChatWindows[long.Parse(tm.receiver)];
+            string messageString = ((MessageANiner.UserControls.MessageHolder)(((((MessageANiner.UserControls.ChatWindow)(chatWindow)).chatMessageArea.Controls[0])))).richTextBoxContent.Text;
+            Assert.AreEqual("0000000001 : ABCD", messageString);
+            this.releaseResources(screen);
+        }
+
+        [TestMethod]
+        public void TestUpdateMessageCount()
+        {
+            HomePage screen = new HomePage();
+            screen.UserDetails = userDetails;
+            messageSpecs.FriendStatusMessage y = new FriendStatusMessage();
+            y.sender = "0000000002";
+            y.UserStatus = FriendStatusMessage.LoggedOnStatus.Offline;
+            screen.updateContacts(y.getMessageString());
+
+            messageSpecs.TextMessage tm = new TextMessage();
+            tm.sender = "0000000001";
+            tm.receiver = "0000000002";
+            tm.TextToSend = "ABCD";
+            screen.openedChatWindowUID = 0000224;
+
+            screen.openedChatWindowUID = 0000224;
+            screen.updateMessages(tm.getMessageString());
+
+            object c = screen.ContactsList.Controls[0];
+            int msgCount = ((MessageANiner.UserControls.ContactControl)(c)).msgCount;
+            Assert.AreEqual(1, msgCount);
+
+
+            screen.openedChatWindowUID = 0000224;
+            tm.TextToSend = "PQRS";
+            screen.updateMessages(tm.getMessageString());
+
+            c = screen.ContactsList.Controls[0];
+            msgCount = ((MessageANiner.UserControls.ContactControl)(c)).msgCount;
+            Assert.AreEqual(2, msgCount);
+            this.releaseResources(screen);
+        }
+
+        public void releaseResources(HomePage screen)
+        {
+            screen.closeLoginScreen();
+            screen.Close();
+            screen.Dispose();
+        }
+    }
+}
---
+++ MessageANinerTestProject/ValidationsClassTests.cs
@@ -0,0 +1,70 @@
+﻿using MessageANiner.Entities;
+using NUnit.Framework;
+using System;
+
+namespace MessageANinerTestProject
+{
+    /// <summary>
+    /// Summary description for ValidationsClassTests
+    /// </summary>
+    [TestFixture]
+    public class ValidationsClassTests
+    {
+
+        [TestCase("Shrujan", null)]
+        [TestCase("", "User name is Mandatory")]
+        public void ShouldValidateUserName(string input, string expectedValue)
+        {
+            var result = Validations.isUserNameValid(input);
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+
+
+        [TestCase("", "New Password required!")]
+        [TestCase("##", "Password invalid!")]
+        [TestCase("AAaa123", null)]
+        public void ShouldValidatePassword(string input, string expectedValue)
+        {
+            var result = Validations.isPasswordValid(input);
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+
+        [TestCase("", "e-mail address is required.")]
+        [TestCase("##", "e-mail address must be valid e-mail address format.\n" +
+                                    "For example 'someone@uncc.edu' ")]
+        [TestCase("skotturi@uncc.edu", null)]
+        public void ShouldValidateEmailID(string input, string expectedValue)
+        {
+            var result = Validations.isEmailIDValid(input);
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+
+        #region 'Setup and TearDown Test Level'
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            Console.WriteLine(" Before : {0}", TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            Console.WriteLine(" After : {0}", TestContext.CurrentContext.Test.Name);
+        }
+
+        [OneTimeSetUp]
+        public void BeforeEachTestFixture()
+        {
+            Console.WriteLine(" Before : {0}", TestContext.CurrentContext.Test.Name);
+        }
+
+        [OneTimeTearDown]
+        public void AfterEachTestFixture()
+        {
+            Console.WriteLine(" After : {0}", TestContext.CurrentContext.Test.Name);
+        }
+
+        #endregion
+    }
+}
